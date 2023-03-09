2e274dddc19dea5fb840b863fd4778a135da01ec
Piyush Kumar <piyush.at.googl@gmail.com>
2023-03-09 12:21:33 +0530

    port changed

--- pm-server/server.js
+++ pm-server/server.js
@@ -12,5 +12,5 @@ app.get('/', (req, res, next) => {
   // });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`));
