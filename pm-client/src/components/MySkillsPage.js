9b6769198a332818173cd3e125374ec0d08bfc61
Piyush Kumar <piyush.at.googl@gmail.com>
2023-03-14 20:52:31 +0530

    registration added

--- pm-client/src/components/MySkillsPage.js
+++ pm-client/src/components/MySkillsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Components from "./Register";
 import { LogoComponent } from '../subComponents/LogoComponent'
 import { SocialIcons } from '../subComponents/SocialIcons'
@@ -13,20 +13,61 @@ export const MySkillsPage = () => {
   const loginForm = React.useRef(null);
   const [slideUp, toggle] = React.useState(false);
 
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    password: "",
+    repassword: "",
+    seller: "0",
+    buyer: "0",
+    designation: "0",
+  })
 
- 
+  function handle(e) {
+    const newData = { ...form };
+    newData[e.target.name] = e.target.value;
+    setForm(newData);
+  }
+
+  async function register() {
+    console.log(form);
+    await fetch("http://localhost:5000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res === 0) {
+        alert("Account created");
+      }
+      else if (res === -1) {
+        alert("Email already exists");
+      }
+      else {
+        alert("Server error");
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return;
+    })
+  }
 
   return (
-
     <React.Fragment>
-    
+
       <Components.GlobalStyle />
       <PowerButton />
-      
+
       <LogoComponent />
-      <SocialIcons  />
+      <SocialIcons />
       <Components.Root>
-      <img src={IMG} />
+        <img src={IMG} />
         <Components.SignupContainer signUp ref={signupForm} slideUp={slideUp}>
           <Components.SignupFormTitle
             ref={toggleSignup}
@@ -40,51 +81,50 @@ export const MySkillsPage = () => {
           <Components.SignupForm slideUp={slideUp}>
             <Components.SignupInput
               type="text"
-              placeholder="Name"
+              placeholder="Full name"
               name="name"
-            
+              onChange={(e) => handle(e)}
             />
             <Components.SignupInput
               type="email"
               placeholder="Email"
               name="email"
-             
+              onChange={(e) => handle(e)}
+            />
+            <Components.SignupInput
+              type="number"
+              placeholder="Contact Number(Whatsapp)"
+              name="phone"
+              onChange={(e) => handle(e)}
+            />
+            <Components.SignupInput
+              type="text"
+              placeholder="Address"
+              name="address"
+              onChange={(e) => handle(e)}
             />
             <Components.SignupInput
               type="password"
               placeholder="Password"
               name="password"
-             
-            />
-                        <Components.SignupInput
-              type="text"
-              placeholder="GST registration details"
-              name="gstregistration"
+              onChange={(e) => handle(e)}
             />
             <Components.SignupInput
-              type="number"
-              placeholder="Pan Card details"
-              name="pancard"
-            />
-            <Components.SignupInput
-              type="number"
-              placeholder="Contact Number"
-              name="contactnumber"
-            />
-            <Components.SignupInput
-              type="text"
-              placeholder="Business Category"
-              name="businesscategory"
+              type="password"
+              placeholder="Re-type password"
+              name="repassword"
+              onChange={(e) => handle(e)}
             />
           </Components.SignupForm>
-          
+
           <Components.SignupButton
             slideUp={slideUp}
+            onClick={register}
           >
             Sign Up
           </Components.SignupButton>
         </Components.SignupContainer>
-        
+
       </Components.Root>
     </React.Fragment>
   );
