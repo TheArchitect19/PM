21fecc6441f193cd88c438bc57758a34f541447b
Aniket Kumar <kraniket123654@gmail.com>
2023-03-18 17:33:02 +0530

    added phone number

--- pm-client/src/App.js
+++ pm-client/src/App.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from "styled-components"
 import { Main } from './components/Main'
 import { AboutPage } from './components/AboutPage'
 import { BlogPage } from './components/BlogPage'
-import { WorkPage } from './components/WorkPage'
+// import { WorkPage } from './components/WorkPage'
 import { MySkillsPage } from './components/MySkillsPage'
 import { Dashboard } from './components/Dashboard'
 import AnimatedCursor from "react-animated-cursor"
@@ -30,7 +30,7 @@ function App() {
       <Route exact path="/" component={Main}/>
       <Route exact path="/about-us" component={AboutPage}/>
       <Route exact path="/services" component={BlogPage}/>
-      <Route exact path="/blog" component={WorkPage}/>
+      {/* <Route exact path="/blog" component={WorkPage}/> */}
       <Route exact path="/register" component={MySkillsPage}/>
       <Route exact path="/profile" component={Dashboard}/>
     </Switch>
