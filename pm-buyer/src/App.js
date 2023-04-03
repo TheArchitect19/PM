42283718da4cefbdbad9080e197e49f42600c16e
Piyush Kumar <piyush.at.googl@gmail.com>
2023-04-03 23:54:08 +0530

    buyer side app added

---
+++ pm-buyer/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COMING SOON</h1>
+      <p>30 days left</p>
+    </div>
+  );
+}
+
+export default App;
