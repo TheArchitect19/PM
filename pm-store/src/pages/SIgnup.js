257b0a273bd3adf116fd951c21aaf8c7723d0dd6
Anurag Deo <anuragjee2020@gmail.com>
2023-06-01 22:31:56 +0530

    login-signup done

---
+++ pm-store/src/pages/SIgnup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavHom from '../components/NavHom'
+import Footer from '../components/Footer'
+import Hero1 from '../components/Hero2'
+
+
+const Home = () => {
+    return (
+      <>
+      <NavHom/>
+      <Hero1 />
+      <Footer/>
+      </>
+    )
+  }
+  
+  export default Home
