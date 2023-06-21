e61360de6c8e4fbea8ca97ab4b3361ad751f0499
Anurag Deo <anuragjee2020@gmail.com>
2023-06-21 10:57:28 +0530

    carousel improved

---
+++ pm-store/src/components/data2.js
@@ -0,0 +1,25 @@
+import multicar1 from "../assets/multicar1.jpg";
+import multicar2 from "../assets/multicar2.jpg";
+import multicar3 from "../assets/multicar3.jpg";
+import multicar4 from "../assets/multicar4.jpg";
+import multicar5 from "../assets/multicar5.jpg";
+import multicar6 from "../assets/multicar6.jpg";
+import multicar7 from "../assets/multicar7.jpg";
+import multicar8 from "../assets/multicar8.jpg";
+import multicar9 from "../assets/multicar9.jpg";
+import multicar10 from "../assets/multicar10.jpg";
+import multicar11 from "../assets/multicar11.jpg";
+
+export const multiData = [
+  multicar1,
+  multicar10,
+  multicar2,
+  multicar3,
+  multicar11,
+  multicar4,
+  multicar5,
+  multicar9,
+  multicar6,
+  multicar7,
+  multicar8,
+];
