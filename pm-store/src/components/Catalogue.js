da0d766e6a104cc105bc58dfe28a15c44e6ae194
Anurag Deo <anuragjee2020@gmail.com>
2023-11-10 11:54:59 +0530

    stashing

--- pm-store/src/components/Catalogue.js
+++ pm-store/src/components/Catalogue.js
@@ -1,53 +1,40 @@
-import { React, useState } from "react";
-import m8 from "../assets/svg/m8.png";
-import m1 from "../assets/svg/m1.png";
-import m2 from "../assets/svg/m2.png";
-import m3 from "../assets/svg/m3.png";
-import m4 from "../assets/svg/m4.png";
-import m5 from "../assets/svg/m5.png";
-import m6 from "../assets/svg/m6.png";
-import m7 from "../assets/svg/m7.png";
-import styles from "./Catalogue.module.css"
-const Navbar = () => {
-  return (
-    <>
-    <div className={styles.colnav}>
-      <div className={styles.mx}>
-    <img className={styles.m8} src={m8} alt="" />
-    <p>Leggings & Churidaar</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m1} alt="" />
-    <p>Ethnic Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m2} alt="" />
-    <p>Casual Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m3} alt="" />
-    <p>Lounge Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m4} alt="" />
-    <p>Active Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m5} alt="" />
-    <p>Formal Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m6} alt="" />
-    <p>Winter Wear</p>
-      </div>
-      <div className={styles.m1}>
-    <img className={styles.m8} src={m7} alt="" />
-    <p>Jeans & Jeggings</p>
-      </div>
-    </div>
-    
-    </>
-  );
-};
+import React from 'react';
+import Marquee from "react-fast-marquee";
+import { SectionWrapper } from "../hoc";
+import { projecti } from "../constants";
 
-export default Navbar;
+const ProjectCard = ({
+    name,
+    image,
+  }) => {
+    return (
+        <div className='px-5' styles={{display:"flex",alignItems:"center",justifyContent:"center",textAlign:"center"}}>
+            <div><img src={image} alt='project_image' style={{width:"80px"}}/></div>
+            <div><h3 className='text-black font-bold' style={{fontFamily:"Poppins",fontWeight:"600",fontSize:"12px"}}>{name}</h3></div>
+        </div>
+    );
+  };
+  
+  const Sponsors = () => {
+    
+    return (
+      <>
+        <Marquee 
+                        gradient={false} 
+                        speed={80} 
+                        pauseOnHover={true}
+                        pauseOnClick={true} 
+                        delay={0}
+                        play={true} 
+                        direction="left"
+                   >
+          {projecti.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} /> 
+          ))}
+          </Marquee>
+      </>
+    );
+  };
+  
+  export default Sponsors;
+  
