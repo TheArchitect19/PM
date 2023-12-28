8855ea38f94fc148591dd0d90065b287ff165d28
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-12-28 22:44:06 +0530

    csrf domain added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     ".pandrimarket.com"
 ]
+CSRF_TRUSTED_ORIGINS=['https://*.pandrimarket.com']
 
 # SESSION_COOKIE_SECURE = False
 # CSRF_COOKIE_SECURE = False
